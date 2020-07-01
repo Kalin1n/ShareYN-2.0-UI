875cc89c8b87c78b4e89d5b937ebf9550527fd7e
Kalin1n <kalinin_ivan_donetsk@ukr.net>
2020-07-01 12:21:58 +0300

    Log out

--- src/Components/Basicpage/index.js
+++ src/Components/Basicpage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import { Layout, Menu, Typography } from 'antd';
+import { Layout, Menu, Typography, Button } from 'antd';
 import "./style.css"
 import { Popover } from "antd";
 const { Header, Content, Footer } = Layout;
@@ -9,7 +9,10 @@ const {Text} = Typography;
 
 function Basicpage(props){
     let token = localStorage.getItem("userToken")
-
+    function logOut(){
+        token = "";
+        localStorage.removeItem("userToken");
+    }
     return(
         <>
             <Layout className="layout">
@@ -20,10 +23,13 @@ function Basicpage(props){
                         <Menu.Item key="2"><Link to="/auth">Authentification</Link></Menu.Item>
                         <Menu.Item key="3"><Link to="/read">Read list</Link></Menu.Item>
                         <Menu.Item key="4"><Link to="/write">Write</Link></Menu.Item>
+            
                     </Menu>
                     {
                     token?
-                        <Text className="status">Authorized  </Text>
+                        <Popover placement="bottomRight" title="Cliclk that to log out" content={<Button onClick={logOut}> Log out</Button>}>
+                            <Text className="status">Authorized  </Text>
+                        </Popover>
                     :  
                         <Popover placement="bottomRight" title="To authorize please wisit the Authorization page" content={<Link to="/auth">Or Click this link</Link>}>
                             <Text className="status">Unauthorized</Text>
